0b2603e667e35ad784444f4cca0babf6a954f899
Timothy Tan <2604749.sit.singaporetech.edu.sg>
2026-09-04 13:07:30 +0800

    Activity 4

--- hello.py
+++ hello.py
@@ -1,12 +1,19 @@
+#Activity 1
 print("==============================")
 print("Welcome here")
 print("My first post")
 print("==============================")
 
+#Activity 2
 username = "cool_creator"
 bio = "Fun Blogger"
 followers = 100
 
+print("Username:", username)
+print("Bio:", bio)
+print("Followers:", followers)
+
+# Activity 3
 followers += 50
 print("Day 1:", followers)
 
@@ -16,6 +23,9 @@ print("Day 2:", followers)
 followers += 10
 print("Day 3:", followers)
 
-print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+#Activity 4
+username = input("Eenter Username: ")
+age = int(input("Enter Age: "))
+category = input("Enter Content Category: ")
+
+
