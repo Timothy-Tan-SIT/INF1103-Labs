6f4ae10e86793bef9342da4020631ab6fcfa1387
Timothy-Tan-SIT <2604749@sit.singaporetech.edu.sg>
2026-09-11 16:55:19 +0800

    Week 2 Lab

---
+++ Week 2 Lab/auditor.py
@@ -0,0 +1,39 @@
+inventory = 0
+entryFailed = 0
+while True:
+
+    userInput = input(
+        'Enter a positive integer below 500 or type "quit" to exit the program: '
+    )
+
+    if userInput.lower() == "quit":
+        print(
+            f"Closing program.\nTotal units processed: {inventory}\nFailed Entries: {entryFailed}"
+        )
+        break
+
+#Check whether input is an integer
+    try:
+        number = int(userInput)
+
+#Check whether input is negative
+        if number <= 0:
+            print("Please key in a positive number.")
+            entryFailed += 1
+
+#Case where inventory exceeds 500; Terminates session
+        elif inventory + number > 500:
+            print(
+                "Adding this amount would exceed the 500-unit limit.\nTerminating session now."
+            )
+            break
+
+#Accepted integer input
+        else:
+            inventory += number
+            print(f"Inventory count is currently: {inventory}")
+
+#Non integer inputs filtered here
+    except ValueError:
+        print("Please input a valid response.")
+        entryFailed += 1
